8dfd302120139cc65d42a3aad5dade4ecebc9d4b
IzabellaAntal <izabellaantal91@yahoo.com>
2018-04-07 18:07:40 +0300

    Primul commit

---
+++ ProiectPCLP/MyForm.h
@@ -0,0 +1,134 @@
+#pragma once
+
+namespace ProiectPCLP {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  loginButton;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->loginButton = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// loginButton
+			// 
+			this->loginButton->BackColor = System::Drawing::SystemColors::Control;
+			this->loginButton->Cursor = System::Windows::Forms::Cursors::Default;
+			this->loginButton->Location = System::Drawing::Point(187, 162);
+			this->loginButton->Name = L"loginButton";
+			this->loginButton->Size = System::Drawing::Size(75, 23);
+			this->loginButton->TabIndex = 0;
+			this->loginButton->Text = L"Login";
+			this->loginButton->UseVisualStyleBackColor = false;
+			this->loginButton->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(153, 86);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(55, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Username";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(227, 79);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(156, 115);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(53, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Password";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(227, 115);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->PasswordChar = '*';
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 4;
+			this->textBox2->UseSystemPasswordChar = true;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(913, 394);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->loginButton);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+
+	}
+	};
+}
